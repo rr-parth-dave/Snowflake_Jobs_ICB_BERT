8a8f3865eddb1661d74c38f737697e9b7f6e8dc9
rr-parth-dave <parth.dave@rakuten.com>
2025-11-18 14:27:06 -0500

    Refactor SQL script for merchant data export
    Updated merchant list and improved variable naming in SQL script.

--- Specific_Merchants_List.sql
+++ Specific_Merchants_List.sql
@@ -2,30 +2,20 @@ USE DATABASE EBATES_PROD;
 
 EXECUTE IMMEDIATE $$
 DECLARE
-    ---------------------------------------------------------
-    -- CONFIGURATION SECTION
-    ---------------------------------------------------------
-    -- 1. Your specific list of Merchant IDs
-    v_merchant_list STRING := '5246, 8333, 7206, 8378, 4548, 10086, 4207, 3993, 16788, 10722, 5487, 896, 13779, 2423, 10752, 4646, 12083, 8850, 13349, 3930, 3866, 98, 4227, 9376, 10199, 8084, 10437, 4767, 8303, 10481, 9142, 13505, 10764, 6646, 16450, 3726, 9800, 9728, 8933, 10151, 12028, 3509, 9265, 13499, 9528, 19013, 9388, 8257';
-    
-    -- 2. How many samples do you want per merchant?
+    -- CONFIGURATION
+    v_merchant_list STRING := '5246, 8333, 7206, 8378, 4548, 10086, 4207';
     v_sample_size INT := 50;
-    
-    -- 3. Temp table name
     v_temp_table STRING := 'TEMP.TEST_TABLE_JOB_SPECIFIC';
 
-    ---------------------------------------------------------
-    -- INTERNAL VARIABLES
-    ---------------------------------------------------------
+    -- INTERNAL VARS
     v_run_ts STRING;
     v_prep_sql STRING;
     v_copy_sql STRING;
     
 BEGIN
-    -- Generate Timestamp
     v_run_ts := TO_CHAR(CURRENT_TIMESTAMP(), 'YYYYMMDD_HH24MISS');
 
-    -- Build the CREATE TABLE query (WITH Merchant Filter)
+    -- 1. CREATE TABLE: We apply the renaming (AS) here
     v_prep_sql := '
     CREATE OR REPLACE TABLE ' || v_temp_table || ' AS
     WITH latest_per_ticket_keys AS (
@@ -59,10 +49,11 @@ BEGIN
         d."TRACKING_TICKET_NUMBER",
         d."UPDATE_TIMESTAMP",
         d."MERCHANT_ID",
-        d."GROUND_TRUTH_NETWORK_ORDER_NUMBER",
-        d."GROUND_TRUTH_NETWORK_SUBTOTAL",
         d."RAW_DOM",
-        d."SANITIZED_DOM"
+        -- RENAMING HAPPENS HERE:
+        d."GROUND_TRUTH_NETWORK_ORDER_NUMBER" AS ground_truth_order_id,
+        d."GROUND_TRUTH_NETWORK_SUBTOTAL"     AS ground_truth_subtotal,
+        d."SANITIZED_DOM"                     AS sanitized_text
     FROM ASSET.AI_OCP_DATASET d
     JOIN top_keys t
         ON d."MERCHANT_ID" = t."MERCHANT_ID"
@@ -77,17 +68,19 @@ BEGIN
 
     EXECUTE IMMEDIATE v_prep_sql;
 
-    -- Build the COPY INTO statement
+    -- 2. EXPORT: We select the NEW names here
     v_copy_sql := '
     COPY INTO @DA_ASSET.DS_FILE_DROP_STAGE/ICB_BERT/run_LIST_' || v_run_ts || '/
     FROM (
-        SELECT TRACKING_TICKET_NUMBER,
+        SELECT 
+               TRACKING_TICKET_NUMBER,
                MERCHANT_ID,
-               GROUND_TRUTH_NETWORK_ORDER_NUMBER,
-               GROUND_TRUTH_NETWORK_SUBTOTAL,
                RAW_DOM,
-               SANITIZED_DOM,
-               UPDATE_TIMESTAMP
+               UPDATE_TIMESTAMP,
+               -- Select the new names
+               ground_truth_order_id,
+               ground_truth_subtotal,
+               sanitized_text
         FROM ' || v_temp_table || '
     )
     PARTITION BY (TO_VARCHAR(MERCHANT_ID))
@@ -96,12 +89,13 @@ BEGIN
         COMPRESSION = NONE
         FIELD_DELIMITER = '',''
         FIELD_OPTIONALLY_ENCLOSED_BY = ''"''
+        HEADER = TRUE 
     )
     MAX_FILE_SIZE = 100000000
     ';
 
     EXECUTE IMMEDIATE v_copy_sql;
 
-    RETURN 'Success: Exported ' || v_sample_size || ' samples for Merchant List to folder run_LIST_' || v_run_ts;
+    RETURN 'Success: Exported ' || v_sample_size || ' samples for Merchant List (Renamed) to run_LIST_' || v_run_ts;
 END;
 $$;
