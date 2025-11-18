e3ea567b91fb66aecff0e6a9ebb316f29a319f8b
rr-parth-dave <parth.dave@rakuten.com>
2025-11-18 14:27:30 -0500

    Update All_Merchants.sql

--- All_Merchants.sql
+++ All_Merchants.sql
@@ -2,27 +2,19 @@ USE DATABASE EBATES_PROD;
 
 EXECUTE IMMEDIATE $$
 DECLARE
-    ---------------------------------------------------------
-    -- CONFIGURATION SECTION
-    ---------------------------------------------------------
-    -- 1. How many samples do you want per merchant? (e.g., 50, 100, 300)
+    -- CONFIGURATION
     v_sample_size INT := 50;
-    
-    -- 2. Temp table name (Safe to leave as is)
     v_temp_table STRING := 'TEMP.TEST_TABLE_JOB_ALL';
 
-    ---------------------------------------------------------
-    -- INTERNAL VARIABLES
-    ---------------------------------------------------------
+    -- INTERNAL VARS
     v_run_ts STRING;
     v_prep_sql STRING;
     v_copy_sql STRING;
     
 BEGIN
-    -- Generate Timestamp
     v_run_ts := TO_CHAR(CURRENT_TIMESTAMP(), 'YYYYMMDD_HH24MISS');
 
-    -- Build the CREATE TABLE query (No Merchant Filter)
+    -- 1. CREATE TABLE: We apply the renaming (AS) here
     v_prep_sql := '
     CREATE OR REPLACE TABLE ' || v_temp_table || ' AS
     WITH latest_per_ticket_keys AS (
@@ -46,7 +38,6 @@ BEGIN
             k."TRACKING_TICKET_NUMBER",
             k."UPDATE_TIMESTAMP"
         FROM latest_per_ticket_keys k
-        -- This limits the rows for EVERY merchant found in the data
         QUALIFY ROW_NUMBER() OVER (
             PARTITION BY k."MERCHANT_ID"
             ORDER BY k."UPDATE_TIMESTAMP" DESC
@@ -56,10 +47,11 @@ BEGIN
         d."TRACKING_TICKET_NUMBER",
         d."UPDATE_TIMESTAMP",
         d."MERCHANT_ID",
-        d."GROUND_TRUTH_NETWORK_ORDER_NUMBER",
-        d."GROUND_TRUTH_NETWORK_SUBTOTAL",
         d."RAW_DOM",
-        d."SANITIZED_DOM"
+        -- RENAMING HAPPENS HERE:
+        d."GROUND_TRUTH_NETWORK_ORDER_NUMBER" AS ground_truth_order_id,
+        d."GROUND_TRUTH_NETWORK_SUBTOTAL"     AS ground_truth_subtotal,
+        d."SANITIZED_DOM"                     AS sanitized_text
     FROM ASSET.AI_OCP_DATASET d
     JOIN top_keys t
         ON d."MERCHANT_ID" = t."MERCHANT_ID"
@@ -74,17 +66,19 @@ BEGIN
 
     EXECUTE IMMEDIATE v_prep_sql;
 
-    -- Build the COPY INTO statement
+    -- 2. EXPORT: We select the NEW names here
     v_copy_sql := '
     COPY INTO @DA_ASSET.DS_FILE_DROP_STAGE/ICB_BERT/run_ALL_' || v_run_ts || '/
     FROM (
-        SELECT TRACKING_TICKET_NUMBER,
+        SELECT 
+               TRACKING_TICKET_NUMBER,
                MERCHANT_ID,
-               GROUND_TRUTH_NETWORK_ORDER_NUMBER,
-               GROUND_TRUTH_NETWORK_SUBTOTAL,
                RAW_DOM,
-               SANITIZED_DOM,
-               UPDATE_TIMESTAMP
+               UPDATE_TIMESTAMP,
+               -- Select the new names
+               ground_truth_order_id,
+               ground_truth_subtotal,
+               sanitized_text
         FROM ' || v_temp_table || '
     )
     PARTITION BY (TO_VARCHAR(MERCHANT_ID))
@@ -93,12 +87,13 @@ BEGIN
         COMPRESSION = NONE
         FIELD_DELIMITER = '',''
         FIELD_OPTIONALLY_ENCLOSED_BY = ''"''
+        HEADER = TRUE  -- Added this so you can see the new names in the file
     )
     MAX_FILE_SIZE = 100000000
     ';
 
     EXECUTE IMMEDIATE v_copy_sql;
 
-    RETURN 'Success: Exported ' || v_sample_size || ' samples for ALL available merchants to folder run_ALL_' || v_run_ts;
+    RETURN 'Success: Exported ' || v_sample_size || ' samples for ALL merchants (Renamed Columns) to run_ALL_' || v_run_ts;
 END;
 $$;
